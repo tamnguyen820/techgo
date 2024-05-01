a498f7504e64969a5abfec5f62f2442d09806539
Tam Nguyen <tam.nguyen8232@gmail.com>
2024-05-01 17:31:41 -0400

    refactor: fetch feeds concurrently

--- internal/rss/fetch.go
+++ internal/rss/fetch.go
@@ -2,6 +2,7 @@ package rss
 
 import (
 	"os"
+	"sync"
 
 	"github.com/mmcdole/gofeed"
 	"gopkg.in/yaml.v3"
@@ -32,13 +33,48 @@ func FetchAllFeeds() ([]*RSSFeed, error) {
 		return nil, err
 	}
 
-	var allFeeds []*RSSFeed
+	// var allFeeds []*RSSFeed
+	// for _, rssFeed := range config.Feeds {
+	// 	feed, err := fetchFeed(rssFeed.URL)
+	// 	if err == nil {
+	// 		allFeeds = append(allFeeds, &RSSFeed{FeedInfo: rssFeed, Feed: feed})
+	// 	}
+	// }
+	// return allFeeds, nil
+
+	allFeeds, err := fetchAllFeedsParallel(config)
+	return allFeeds, err
+}
+
+func fetchAllFeedsParallel(config Config) ([]*RSSFeed, error) {
+	var wg sync.WaitGroup
+	feedChan := make(chan *RSSFeed)
+
+	// Spawn goroutines to fetch each feed concurrently
 	for _, rssFeed := range config.Feeds {
-		feed, err := fetchFeed(rssFeed.URL)
-		if err == nil {
-			allFeeds = append(allFeeds, &RSSFeed{FeedInfo: rssFeed, Feed: feed})
-		}
+		wg.Add(1)
+		go func(rssFeed RSSFeedInfo) {
+			defer wg.Done()
+			feed, err := fetchFeed(rssFeed.URL)
+			if err != nil {
+				return
+			}
+			feedChan <- &RSSFeed{FeedInfo: rssFeed, Feed: feed}
+		}(rssFeed)
 	}
+
+	// Aggregate fetched feeds
+	var allFeeds []*RSSFeed
+	go func() {
+		for feed := range feedChan {
+			allFeeds = append(allFeeds, feed)
+		}
+	}()
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+	close(feedChan)
+
 	return allFeeds, nil
 }
 
